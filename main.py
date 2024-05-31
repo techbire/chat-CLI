4e89dbf6953aa840cdaf492170a399d1b2ad1442
Ansh Gupta <techbire69@gmail.com>
2024-05-31 10:49:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import google.generativeai as genai
 API_KEY = '7185632630:AAG2E9D7xxlYvIOG89E2NojgN68aQC5drBI'
 model=genai.GenerativeModel('gemini-pro')
 chat=model.start_chat(history=[])
-instruction="Respond in this way like you are explaining things in two to three with concise and use of easy word."
+instruction="Respond in this way like you are explaining things in two to three with concise and use of easy words."
 while(true):
-  question=input("You: ")
+  question=input("YOU: ")
   if(question.strip()==''):
     break
-    response=chat.send_message(intruction + question)
-    print('\n')
-    print(f"techbire: {response.text}")
-    print('\n')
+    response=chat.send_message(instruction + question)
+print('\n')
+print(f"BOT: {response.text}")
+print('\n')
     
     
